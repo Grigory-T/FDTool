b118bbe87b40d859d178b15fe3864e5cff263882
Kristian Schultz <kristian.schultz@uni-rostock.de>
2024-03-14 16:03:12 +0100

    Added parallel processing for set counting.

--- fdtool/modules/GetFDs.py
+++ fdtool/modules/GetFDs.py
@@ -1,14 +1,21 @@
+from multiprocessing import Pool
 from . import binaryRepr
+import time
+
+global globalTimer
+globalTimer = 0.0
+global globalCount
+globalCount = 0
 
 # Create decorator function to see how many times functions are called
 def call_counter(func):
     
     def helper(*args, **kwargs):
         helper.calls += 1
-        return func(*args, **kwargs);
+        return func(*args, **kwargs)
     helper.calls = 0
     helper.__name__= func.__name__
-    return helper;
+    return helper
 
 # Calculate Partition (C_k, r(U)) - the partitions
 #                                   of each candidate at level k are calculated
@@ -17,11 +24,15 @@ def call_counter(func):
 
 @call_counter
 def CardOfPartition(Candidate, df):
+    global globalCount
+    globalCount += 1
 
     # If length is one, find number of unique elements in column
-    if len(Candidate) == 1: return df[Candidate[0]].nunique()
+    if len(Candidate) == 1:
+        return df[Candidate[0]].nunique()
     # If length is +1, create groups over which to find number of unique elements
-    else: return df.drop_duplicates(Candidate).count().iloc[0];
+    else:
+        return df.drop_duplicates(Candidate).count().iloc[0]
 
 # Obtain FDs(C_km1) - checks the FDs of each
 #                     candidate X in C_k
@@ -37,33 +48,54 @@ def CardOfPartition(Candidate, df):
 #           X* = X *Union* {v_i}
 #           F = F *Union* {X -> v_i}    \\Theorem 2
 #       }
-#   return (F);
+#   return (F)
+
+def showStats():
+    global globalCount
+    global globalTimer
+    print(f"time: {globalTimer}")
+    print(f"counts: {globalCount}")
+
+def action(x):
+    CardOfPartition(x[0], x[1])
 
 def f(C_km1, df, Closure, U, Cardinality):
+    global globalTimer
 
     # Set F to null list; Initialize U_c to remaining columns in data frame
-    F = []; U_c = list(df.head(0));
+    F = []
+    U_c = list(df.head(0))
     
     # Identify the subsets whose cardinality of partition should be tested
-    SubsetsToCheck = [list(Subset) for Subset in set([frozenset(Candidate + [v_i]) for Candidate in C_km1 for v_i in list(set(U_c).difference(Closure[binaryRepr.toBin(Candidate, U)]))])];
+    SubsetsToCheck = [list(Subset)
+        for Subset in set([frozenset(Candidate + [v_i])
+        for Candidate in C_km1
+        for v_i in list(set(U_c).difference(Closure[binaryRepr.toBin(Candidate, U)]))
+        ])]
     
     # Add singleton set to SubsetsToCheck if on first k-level
-    if len(C_km1[0]) == 1: SubsetsToCheck += C_km1;
+    if len(C_km1[0]) == 1:
+        SubsetsToCheck += C_km1
     
-    # Iterate through subsets mapped to the Cardinality of Partition function
-    for Cand, Card in zip(SubsetsToCheck, map(CardOfPartition, SubsetsToCheck, [df]*len(SubsetsToCheck))):
-        # Add Cardinality of Partition to dictionary
-        Cardinality[binaryRepr.toBin(Cand, U)] = Card;
+    s = time.time()
+    with Pool() as pool:
+
+        # Iterate through subsets mapped to the Cardinality of Partition function
+        for Cand, Card in zip(SubsetsToCheck, pool.map(action, zip(SubsetsToCheck, [df] * len(SubsetsToCheck)))):
+            # Add Cardinality of Partition to dictionary
+            Cardinality[binaryRepr.toBin(Cand, U)] = Card
+    globalTimer = time.time() - s
 
     # Iterate through candidates of C_km1
     for Candidate in C_km1:
+        candidateBinRepr = binaryRepr.toBin(Candidate, U)
         # Iterate though attribute subsets that are not in U - X{+}; difference b/t U and inclusive closure of candidate    
-        for v_i in list(set(U_c).difference(Closure[binaryRepr.toBin(Candidate, U)])):
+        for v_i in list(set(U_c).difference(Closure[candidateBinRepr])):
             # Check if the cardinality of the partition of {Candidate} is equal to that of {Candidate, v_i}
-            if Cardinality[binaryRepr.toBin(Candidate, U)] == Cardinality[binaryRepr.toBin(Candidate + [v_i], U)]:
+            if Cardinality[candidateBinRepr] == Cardinality[binaryRepr.toBin(Candidate + [v_i], U)]:
                 # Add attribute v_i to closure
-                Closure[binaryRepr.toBin(Candidate, U)].add(v_i)
+                Closure[candidateBinRepr].add(v_i)
                 # Add list (Candidate, v_i) to F
-                F.append([tuple(Candidate), v_i]);
+                F.append([tuple(Candidate), v_i])
     
-    return Closure, F, Cardinality;
+    return Closure, F, Cardinality
