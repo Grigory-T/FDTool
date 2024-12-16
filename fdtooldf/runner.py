be57e8d611871864a7136cdb47ae0986fd29aa61
Grigory-T <trofimov6409@gmail.com>
2024-12-16 22:29:05 +0300

    correct print results (order of elements)

--- fdtooldf/runner.py
+++ fdtooldf/runner.py
@@ -4,7 +4,7 @@ from .fdtool import main
 from .utils import convert_col_name_back, result_dict_to_str
 
 
-def run_fdtool(df, max_time=30, max_k_level=15):
+def run_fdtool(df, max_time=30, max_k_level=15, cut=10):
     df = df.rename(lambda el: str(el), axis=1)
     if any(True for c in df.columns if c == ""):
         raise Exception("empty string in columns NOT allowed")
@@ -31,7 +31,7 @@ def run_fdtool(df, max_time=30, max_k_level=15):
     # Retrieve the result if the process completed successfully
     if not result_queue.empty():
         result_dict = convert_col_name_back(result_queue.get(), map_id_colname)
-        result_str = result_dict_to_str(result_dict, map_colname_id)
+        result_str = result_dict_to_str(result_dict, map_colname_id, cut)
         return (result_str, result_dict)
     else:
         print("No result returned (process may have been terminated).")
--- fdtooldf/utils.py
+++ fdtooldf/utils.py
@@ -1,45 +1,69 @@
-def result_dict_to_str(d, m):
+def result_dict_to_str(d, m, cut):
+    """
+    pretty printing
+    ensure to sort elements in some logical order
+    """
     s = ""
-    n = 10
 
     s += ">>> FD (functional dependancies):\n"
-    l = []
-    for vs, v in d["FD"]:
-        ss = ""
-        ss += " | ".join(el[:n] for el in sorted(vs, key=m.get))
-        ss += " -> "
-        ss += str(v)
-        l.append(ss)
-    s += "\n".join(sorted(l, key=lambda el: (len(el), el)))
-    s += "\n\n"
+    temp = sorted(
+        ((sorted(el1, key=m.get), el2) for el1, el2 in d["FD"]),
+        key=lambda el: [len(el[0])] + el[0] + [el[1]],
+    )
+    for el1, el2 in temp:
+        s += " | ".join(el[:cut] for el in el1)
+        s += " -> "
+        s += el2[:cut]
+        s += "\n"
+    s += "\n"
 
     s += ">>> EQ (equivalences):\n"
-    for el in sorted(d["EQ"], key=lambda el: len(el[0]) + len(el[1])):
-        s += " | ".join(el[:n] for el in sorted(el)[0])
+    temp = sorted(
+        (
+            (
+                [el[:cut] for el in sorted(el1, key=m.get)],
+                [el[:cut] for el in sorted(el2, key=m.get)],
+            )
+            for el1, el2 in d["EQ"]
+        ),
+        key=lambda el: [len(el[0] + el[1])] + el[0] + el[1],
+    )
+    for el1, el2 in temp:
+        s += " | ".join(el[:cut] for el in el1)
         s += " <-> "
-        s += " | ".join(el[:n] for el in sorted(el)[1])
+        s += " | ".join(el[:cut] for el in el2)
         s += "\n"
     s += "\n"
 
     s += ">>> CK (candidate keys):\n"
-    for el in sorted(d["CK"], key=lambda el: (len(el), sorted(el))):
-        s += " | ".join(el[:n] for el in sorted(el))
+    temp = sorted(
+        (sorted(el, key=m.get) for el in d["CK"]), key=lambda el: [len(el)] + el
+    )
+    for el in temp:
+        s += " | ".join(el[:cut] for el in el)
         s += "\n"
-    s = s[:-1]
 
     return s
 
 
 def convert_col_name_back(d, m):
+    """
+    if order does not matter - change sequence to frozenset
+    change columns name back to normal
+    """
 
-    d["FD"] = frozenset((frozenset(m[el] for el in vs), m[v]) for vs, v in d["FD"])
+    d["FD"] = frozenset(
+        (frozenset(m[el] for el in el1), m[el2]) for el1, el2 in d["FD"]
+    )
 
     d["EQ"] = frozenset(
-        (
-            frozenset(m[el] for el in sorted(el)[0]),
-            frozenset(m[el] for el in sorted(el)[1]),
+        frozenset(
+            [
+                frozenset(m[el] for el in el1),
+                frozenset(m[el] for el in el2),
+            ]
         )
-        for el in d["EQ"]
+        for el1, el2 in d["EQ"]
     )
 
     d["CK"] = frozenset(frozenset(m[v] for v in el) for el in d["CK"])
